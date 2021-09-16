b3f8aabd1ed7db8a250ac2cc7b71edf8efe2f8a0
unknown <aki.jazz1027@gmail.com>
2021-09-17 04:51:09 +0900

    自分で作ったデータでchartが描画できた。asyncでの非同期処理はだいぶダサい

--- app/javascript/components/Chart.vue
+++ app/javascript/components/Chart.vue
@@ -1,34 +1,90 @@
-  <div id="app">
-    <button @click="check"></button>
-  </div>
+<template>
+  <line-chart v-if="loaded" :dateArray="dateArray" :durationArray="durationArray"/>
+</template>
 
 <script>
-import {Bar} from 'vue-chartjs';
+import LineChart from "./LineChart.vue";
 
 export default {
-  extends: Bar,
-  mounted () {
-    // this.$store.dispatch('getData')
-    this.renderChart(this.storeMovies)
+  components: {
+    LineChart
+  },
+  async mounted () {
+    this.$store.commit('getData')
+    await this.fillData();
+    this.loaded = true
+  },
+  data: function () {
+    return {
+      loaded: false,
+      duration: {},
+    }
   },
   computed: {
-    totalDuration: function() {
-      let sum = 0;
-      for(let id = 0; id < this.storeMovies.length; id++) {
-        sum += this.storeMovies[id].duration
-      }
-      return sum;
-    },
     storeMovies: function() {
       return this.$store.getters.storeMovies;
-    }
+    },
+    dateArray: function() {
+      let dateArray = [];
+      for(let num = 89; num >= 0; num--) {
+        const date = new Date;
+        date.setDate(date.getDate() - num)
+        let year = String(date.getFullYear());
+        let month = String(date.getMonth() + 1);
+        if(month.length == 1) {
+          month = 0 + month
+        };
+        let day = String(date.getDate());
+        if(day.length == 1) {
+          day = 0 + day
+        };
+        const fitToday = year + '-' +  month + '-' + day
+        dateArray.push(fitToday);
+      }
+      return dateArray
+    },
+    durationArray: function() {
+      let filteredStoreMovies = [];
+      this.storeMovies.forEach(element => {
+        const obj = {};
+        if(element.date != null) {
+          obj.date = element.date.slice(0, 10)
+        }else{
+          obj.date = element.date
+        }
+        obj.duration = element.duration
+        filteredStoreMovies.push(obj);
+      })
+      let filteredDateArray = [];
+      this.dateArray.forEach(element => {
+        let obj = {};
+        obj.date = element;
+        obj.duration = 0;
+        filteredDateArray.push(obj)
+      })
+      filteredStoreMovies.forEach(store => {
+        filteredDateArray.forEach(date => {
+          if(date.date == store.date) {
+            date.duration += store.duration
+          }
+        })
+      })
+      let durationArray = [];
+      filteredDateArray.forEach(date => {
+        durationArray.push(date.duration)
+      })
+      return durationArray;
+    },
   },
   methods: {
-    check: function() {
-      console.log(this.storeMovies)
+    fillData: function() {
+      return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('a');
+        }, 2000);
+    })
     }
   }
-  
 }
 </script>
 
---
+++ app/javascript/components/LineChart.vue
@@ -0,0 +1,28 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  props: ['dateArray', 'durationArray'],
+  mounted() {
+    this.renderChart({
+      labels: this.dateArray, 
+      datasets: [{
+        label: '再生時間',
+        data: this.durationArray,
+        backgroundColor: "transparent",
+        borderColor: "rgba(1, 116, 188, 0.50)",
+        pointBackgroundColor: "rgba(171, 71, 188, 1)"
+      }]},
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: "My Data"
+        }
+      }
+    );
+  }
+};
+</script>
