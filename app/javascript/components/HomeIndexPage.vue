c851bbd9c65db2106bc97f3ccd9dcdeae77a51ed
unknown <aki.jazz1027@gmail.com>
2021-09-30 03:00:30 +0900

    HomeIndexPageのレイアウトをきれいにしている2

--- app/javascript/components/HomeIndexPage.vue
+++ app/javascript/components/HomeIndexPage.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app id="app">
-    <v-container id="typography-view" fluid tag="section">
+    <v-container id="typography-view" fluid tag="section" class="grey lighten-3">
 
       <v-row align="center" justify="center" class="display-4 my-5 pa-5">
         <v-card class="pa-5">
-          <p class="display-2 mb-1">合計視聴時間</p>
+          <p class="display-1 mb-7">合計視聴時間</p>
           <span v-if="totalDuration >= 3600">
             {{Math.floor(totalDuration/3600)}}時間
           </span>
@@ -13,34 +13,41 @@
           </span>{{totalDuration%60}}秒
         </v-card>
       </v-row>
+      
+      <v-card class="mx-10 pa-5">
 
-      <v-row v-for="movie in storeMovies" :key="movie.id"  class="my-5">
-        <v-card class="mx-auto">
-          <v-col cols="4">
-            <router-link :to="{ name: 'MovieDetailPage', params: { id: movie.id } }">
-            <img :src="movie.thumbnail" width="480" height="360">
-            <p class="headline">
-              <span v-if="movie.duration >= 3600">
-                {{Math.floor(movie.duration/3600)}}時間
-              </span>
-              <span v-if="movie.duration >= 60">
-                {{Math.floor(movie.duration/60%60)}}分
-              </span>{{movie.duration%60}}秒
-            </p>
-            </router-link>
-          </v-col>
-          <v-col cols="8" class="display-2">
-              <router-link :to="{ name: 'MovieDetailPage', params: { id: movie.id } }">{{movie.title}}</router-link>
-              <span class="headline">1時間前</span>
-              <v-devider></v-devider>
-              <!-- <span class="headline">{{movie.created_at}}</span> -->
-              <p>{{movie.comment}}</p>
-          </v-col>
+        <v-card v-for="movie in storeMovies" :key="movie.id" width="80%" class="mx-10 my-10">
+          <v-row justify="center"> 
+
+            <v-col cols="4" class="d-flex justify-center">
+              <v-img :src="movie.thumbnail" :aspect-ratio="4/3" :width="300">
+                <v-row align="end" justify="end">
+                  <v-col cols="3" class="black white--text headline d-flex justify-center">
+                    <span v-if="movie.duration >= 3600">
+                      {{Math.floor(movie.duration/3600)}}時間
+                    </span>
+                    <span v-if="movie.duration >= 60">
+                      {{Math.floor(movie.duration/60%60)}}分
+                    </span>{{movie.duration%60}}秒
+                  </v-col>
+                </v-row>
+              </v-img>
+            </v-col>
+
+            <v-col cols="8" class="display-2">
+                <router-link :to="{ name: 'MovieDetailPage', params: { id: movie.id } }">{{movie.title}}</router-link>
+                <span class="headline">1時間前</span>
+                <v-divider></v-divider>
+                <!-- <span class="headline">{{movie.created_at}}</span> -->
+                <p>{{movie.comment}}</p>
+            </v-col>
+
+          </v-row>
         </v-card>
-      </v-row>
-    <router-link to="/movies/new" >新しい動画を追加</router-link>
-    <button @click="check"></button>
 
+      </v-card>
+      <router-link to="/movies/new" >新しい動画を追加</router-link>
+      <button @click="check"></button>
     </v-container>
   </v-app>
 </template>
