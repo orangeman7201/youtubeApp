2c57d3bb6fe0245a9dfb05ecad81f77c2565b01c
unknown <aki.jazz1027@gmail.com>
2021-09-30 19:37:38 +0900

    HomeIndexPageを修正　サイドバーの作成に移る前

--- app/javascript/components/HomeIndexPage.vue
+++ app/javascript/components/HomeIndexPage.vue
@@ -15,12 +15,21 @@
       </v-row>
       
       <v-card class="mx-10 pa-5">
+        <v-row align="center" justify="end">
+          <v-col cols="1" >
+            <v-btn fab class="green accent-3 white--text">
+              <v-icon class="display-3">mdi-plus</v-icon>
+            </v-btn>
+              <span class="display-2">追加</span>
+          </v-col>
+        </v-row>
+        <!-- <router-link to="/movies/new" >新しい動画を追加</router-link> -->
 
-        <v-card v-for="movie in storeMovies" :key="movie.id" width="80%" class="mx-10 my-10">
-          <v-row justify="center"> 
+        <v-card v-for="movie in storeMovies" :key="movie.id" width="97%" class="ma-10 pa-15" elevation="7">
+          <v-row> 
 
             <v-col cols="4" class="d-flex justify-center">
-              <v-img :src="movie.thumbnail" :aspect-ratio="4/3" :width="300">
+              <v-img :src="movie.thumbnail" :aspect-ratio="4/3">
                 <v-row align="end" justify="end">
                   <v-col cols="3" class="black white--text headline d-flex justify-center">
                     <span v-if="movie.duration >= 3600">
@@ -35,40 +44,77 @@
             </v-col>
 
             <v-col cols="8" class="display-2">
-                <router-link :to="{ name: 'MovieDetailPage', params: { id: movie.id } }">{{movie.title}}</router-link>
-                <span class="headline">1時間前</span>
+              <div class="ma-2">
+                <h4>{{movie.title}}
+                  <span class="headline" >1時間前</span>
+                </h4>
+              </div>
                 <v-divider></v-divider>
-                <!-- <span class="headline">{{movie.created_at}}</span> -->
+              <div class="ma-3">
                 <p>{{movie.comment}}</p>
+              </div>
             </v-col>
 
           </v-row>
         </v-card>
 
       </v-card>
-      <router-link to="/movies/new" >新しい動画を追加</router-link>
       <button @click="check"></button>
     </v-container>
   </v-app>
 </template>
 
 <script>
+  // import { mdiPlusCircle } from '@mdi/js';
 
 export default {
-  mounted () {
-    this.$store.dispatch('getData')
+  data: function () {
+    return {
+      storeMovies: [
+    {
+        "id": 9,
+        "url": "https://www.youtube.com/watch?v=2F1c3colJ7k",
+        "duration": 528,
+        "title": "新入生歓迎会に無関係の人間がガチで潜入してみた",
+        "comment": "おもしろかった4",
+        "date": null,
+        "thumbnail": "https://i.ytimg.com/vi/2F1c3colJ7k/sddefault.jpg",
+        "created_at": "2021-09-27T17:02:49.948Z",
+        "updated_at": "2021-09-27T17:02:49.948Z",
+        "user_id": 3
+    },
+    {
+        "id": 12,
+        "url": "https://www.youtube.com/watch?v=PN0ohcyn5sA",
+        "duration": 28948,
+        "title": "4K 焚き火のパチパチ音と湧き水のちょろちょろ音８時間 “ Bonfire and spring water”",
+        "comment": "おもしろかった4",
+        "date": null,
+        "thumbnail": "https://i.ytimg.com/vi/PN0ohcyn5sA/sddefault.jpg",
+        "created_at": "2021-09-29T04:20:04.298Z",
+        "updated_at": "2021-09-29T04:20:04.298Z",
+        "user_id": 3
+    },
+    {
+        "id": 13,
+        "url": "https://www.youtube.com/watch?v=2F1c3colJ7k",
+        "duration": 528,
+        "title": "新入生歓迎会に無関係の人間がガチで潜入してみた",
+        "comment": "おもしろかったおもしろかったおもしろかったおもしろかったおもしろかったおもしろかったおもしろかったおもしろかったおもしろかったおもしろかった",
+        "date": null,
+        "thumbnail": "https://i.ytimg.com/vi/2F1c3colJ7k/sddefault.jpg",
+        "created_at": "2021-09-29T07:09:00.158Z",
+        "updated_at": "2021-09-29T07:09:20.182Z",
+        "user_id": 3
+    }
+]
+    }
   },
   computed: {
     totalDuration: function() {
-      let sum = 0;
-      for(let id = 0; id < this.storeMovies.length; id++) {
-        sum += this.storeMovies[id].duration
-      }
+      let sum = 72010;
       return sum;
     },
-    storeMovies: function() {
-      return this.$store.getters.storeMovies;
-    }
   },
   methods: {
     check: function() {
