f126b3e3aa8d83043408b0a3d18ba1670f0d07b3
unknown <aki.jazz1027@gmail.com>
2021-10-03 16:21:50 +0900

    chartjs修正中2

--- app/javascript/components/LineChart.vue
+++ app/javascript/components/LineChart.vue
@@ -7,6 +7,7 @@ export default {
   
   mounted () {
     this.getData();
+    console.log(this.oneWeekDurationArray)
     this.renderChart(this.chartData, this.options)
   },
   data: function () {
@@ -18,44 +19,6 @@ export default {
     }
   },
   computed: {
-    fourWeekDateArray: function() {
-      let dateArray = [];
-      for(let num = 29; num >= 0; num--) {
-        const date = new Date;
-        date.setDate(date.getDate() - num)
-        let year = String(date.getFullYear());
-        let month = String(date.getMonth() + 1);
-        if(month.length == 1) {
-          month = 0 + month
-        };
-        let day = String(date.getDate());
-        if(day.length == 1) {
-          day = 0 + day
-        };
-        const fitToday = year + '-' +  month + '-' + day
-        dateArray.push(fitToday);
-      }
-      return dateArray
-    },
-    twoWeekDateArray: function() {
-      let dateArray = [];
-      for(let num = 13; num >= 0; num--) {
-        const date = new Date;
-        date.setDate(date.getDate() - num)
-        let year = String(date.getFullYear());
-        let month = String(date.getMonth() + 1);
-        if(month.length == 1) {
-          month = 0 + month
-        };
-        let day = String(date.getDate());
-        if(day.length == 1) {
-          day = 0 + day
-        };
-        const fitToday = year + '-' +  month + '-' + day
-        dateArray.push(fitToday);
-      }
-      return dateArray
-    },
     oneWeekDateArray: function() {
       let dateArray = [];
       for(let num = 6; num >= 0; num--) {
@@ -79,57 +42,26 @@ export default {
       const durationArray = [];
       const stepArray = []
       this.oneWeekDateArray.forEach(date => {
-        const stepObj = {date: date, duration: 0}
+        const stepObj = {x: date, y: 0}
         stepArray.push(stepObj)
       })
-      stepArray.forEach(stepObj => {
-        this.movies.forEach(movie => {
-          if(stepObj.date === movie.created_at.slice(0, 10)) {
-            stepObj.duration += movie.duration
-          }
+      this.movies.forEach(element => {
+          console.log(element)
         })
-        durationArray.push(stepObj.duration)
-      })
-      return durationArray
-    },
-    twoWeekDurationArray: function() {
-      const durationArray = [];
-      const stepArray = []
-      this.twoWeekDateArray.forEach(date => {
-        const stepObj = {date: date, duration: 0}
-        stepArray.push(stepObj)
-      })
       stepArray.forEach(stepObj => {
-        this.movies.forEach(movie => {
-          if(stepObj.date === movie.created_at.slice(0, 10)) {
-            stepObj.duration += movie.duration
-          }
-        })
-        durationArray.push(stepObj.duration)
-      })
-      return durationArray
-    },
-    fourWeekDurationArray: function() {
-      const durationArray = [];
-      const stepArray = []
-      this.fourWeekDateArray.forEach(date => {
-        const stepObj = {date: date, duration: 0}
-        stepArray.push(stepObj)
-      })
-      stepArray.forEach(stepObj => {
-        this.movies.forEach(movie => {
-          if(stepObj.date === movie.created_at.slice(0, 10)) {
-            stepObj.duration += movie.duration
-          }
-        })
-        durationArray.push(stepObj.duration)
+        // this.movies.forEach(movie => {
+        //   console.log(movie)
+        //   // if(stepObj.x === movie.created_at.slice(0, 10)) {
+        //   //   stepObj.y += movie.duration
+        //   //   console.log('if文中')
+        //   // }
+        // })
+        durationArray.push(stepObj)
       })
       return durationArray
     },
     chartData: function() {
       const chartdata = {
-        datacollection: {
-          labels: this.oneWeekDateArray,
           datasets: [
             {
               label: 'test1',
@@ -137,7 +69,6 @@ export default {
               data: this.oneWeekDurationArray
             }
           ]
-        }
       }
       return chartdata
     },
@@ -156,9 +87,6 @@ export default {
           console.log(error)
       })
     },
-    check: function() {
-      console.log(this.chartData)
-    }
   }
 }
 </script>
