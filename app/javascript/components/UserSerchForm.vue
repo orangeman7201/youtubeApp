737060676ac15d95ca003480b1ef80a8042ae184
unknown <aki.jazz1027@gmail.com>
2021-10-27 23:42:47 +0900

    微修正

--- app/javascript/components/UserSerchForm.vue
+++ app/javascript/components/UserSerchForm.vue
@@ -1,24 +1,27 @@
 <template>
   <v-container fill-height class="grey lighten-3">
-    <v-card >
-      <v-form @submit.prevent="submitData" class="ma-5"> 
-          <v-row>
-            <v-col cols="12" v-if="error !== ''">
-              <p class="red--text mt-5 text-h6">ユーザーが見つかりません</p>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                v-model="inputUserName.userName"
-                label="ユーザー名"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" class="d-flex justify-center">
-              <v-btn type="submit" class="white--text green accent-3 mb-5">検索</v-btn>
-            </v-col>
-          </v-row>
-      </v-form>
-    </v-card>
+    <v-row align="center" justify="center">
+
+      <v-card>
+        <v-form @submit.prevent="submitData" class="ma-5"> 
+            <v-row align="center" justify="center">
+              <v-col cols="12" v-if="error !== ''">
+                <p class="red--text mt-5 text-h6">ユーザーが見つかりません</p>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="inputUserName.userName"
+                  label="ユーザー名"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" class="d-flex justify-center">
+                <v-btn type="submit" class="white--text green accent-3 mb-5">検索</v-btn>
+              </v-col>
+            </v-row>
+        </v-form>
+      </v-card>
+    </v-row>
   </v-container>
 </template>
 
