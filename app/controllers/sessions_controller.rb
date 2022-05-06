13fa9a7862df62b1ac253467a4f8cb56be747a8d
orangeman7201 <aki.jazz1027@gmail.com>
2022-05-06 23:42:45 +0900

    ログインログアウトをdevise化

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # helperに詳細記述。sessionメソッドにUser.idを渡している
-      log_in(user)
-      remember(user)
+      sign_in(user)
     else 
       render json:{ errors: user.errors.full_messages }, status: :unprocessable_entity 
     end
   end
 
   def destroy
-    log_out
+    sign_out(current_user)
   end
 
   def gest_login
