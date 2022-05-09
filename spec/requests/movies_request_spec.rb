9b778fd9906375257a6d85f1e928a032d22fa3a3
orangeman7201 <aki.jazz1027@gmail.com>
2022-05-09 23:51:37 +0900

    テストの修正

--- spec/requests/movies_request_spec.rb
+++ spec/requests/movies_request_spec.rb
@@ -10,16 +10,15 @@ RSpec.describe "moviesコントローラーのテスト", type: :request do
       let!(:movies) { create_list(:movie, 11, user: @user) }
       before do
         get movies_path
-        # let!(:response) { JSON.parse(response.body) }
       end
       it 'リクエストは200 OKとなること' do
         expect(response.status).to eq 200
       end
       it '10件動画が返却されること' do
-        expect(response.body.length).to eq 10
+        expect(JSON.parse(response.body).length).to eq 10
       end
       it '1responseが作成日時の新しい順にならんでいること' do
-        expect(JSON.parse(response.body)[0]["created_at"]).to eq movies.last.created_at
+        expect(JSON.parse(response.body)[0]["id"]).to eq movies.last.id
       end
     end
   end
