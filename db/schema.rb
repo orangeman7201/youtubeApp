dcccd0dd21eb907b095b157b6782f55da5e7d4d5
orangeman7201 <aki.jazz1027@gmail.com>
2022-06-25 15:45:15 +0900

    制限時間をlimitに変更

--- db/schema.rb
+++ db/schema.rb
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 2022_06_22_143709) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "remember_token"
-    t.integer "target_time"
+    t.integer "limit"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
