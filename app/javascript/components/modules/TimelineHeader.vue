f7e74a88b68cc72f2a0ee51e45af9cf0be9fa41d
orangeman7201 <aki.jazz1027@gmail.com>
2022-07-17 02:08:11 +0900

    ボタン付きのヘッダーコンポーネントを作成

--- app/javascript/components/modules/TimelineHeader.vue
+++ app/javascript/components/modules/TimelineHeader.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <h1>test</h1>
+  <div class="header">
+    <div class="button-group">
+      <button class="mr-2" :class="friendPostsButtonClass" @click="showFriendPosts">
+        友達の投稿
+      </button>
+      <button class="ml-2" :class="myPostsButtonClass" @click="showMyPosts">
+        自分の投稿
+      </button>
+    </div>
   </div>
 </template>
 
@@ -10,7 +17,51 @@ export default {
   data: function() {
     return {
       user: {},
+      selected: 'me'
     }
   },
+  computed: {
+    myPostsButtonClass: function() {
+      return this.selected === 'me' ? 'showed-posts v-btn--outlined' : 'unshowed-posts'
+    },
+    friendPostsButtonClass: function() {
+      return this.selected === 'friend' ? 'showed-posts v-btn--outlined' : 'unshowed-posts'
+    },
+  },
+  methods: {
+    showFriendPosts: function() {
+      this.selected = 'friend'
+    },
+    showMyPosts: function() {
+      this.selected = 'me'
+    },
+  }
 }
 </script>
+
+<style scoped>
+.header {
+  width: 100%;
+  padding: 16px 0;
+  background-color: #A7DDEA;
+  font-size: 14px;
+  font-weight: 400px;
+}
+.button-group {
+  display: flex;
+  justify-content: center;
+}
+.unshowed-posts {
+  padding: 8px 32px;
+  border-radius: 28px;
+  border: solid 2.5px #FFFFFF;
+  color: #FFFFFF;
+}
+.showed-posts {
+  padding: 8px 32px;
+  border-radius: 28px;
+  background-color: #FFFFFF;
+  color: #18B1CE;
+  border: none;
+}
+</style>
