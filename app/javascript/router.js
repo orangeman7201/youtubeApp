a97f209f5335a9910d7fe06d07f83e209dc9d2ea
orangeman7201 <aki.jazz1027@gmail.com>
2022-08-14 15:29:00 +0900

    不要なコンポーネントを削除

--- app/javascript/router.js
+++ app/javascript/router.js
@@ -3,8 +3,6 @@ import Router from 'vue-router';
 import HomeIndexPage from './components/HomeIndexPage.vue';
 import MovieDetailPage from './components/MovieDetailPage.vue';
 import InputFormPage from './components/InputFormPage.vue';
-import Chart from './components/Chart.vue';
-import ChartMethods from './components/ChartMethods.vue';
 import SignUpForm from './components/SignUpForm.vue';
 import LoginForm from './components/LoginForm.vue';
 import UserDetailPage from './components/UserDetailPage.vue';
@@ -24,8 +22,6 @@ export default new Router ({
      {path: '/home', name: 'HomeIndexPage', component: HomeIndexPage}, 
      {path: '/movies/:id(\\d+)', name: 'MovieDetailPage',component: MovieDetailPage}, 
      {path: '/movies/new', component: InputFormPage}, 
-     {path: '/movies/chart', component: Chart}, 
-     {path: '/movies/chartmethods', component: ChartMethods}, 
      {path: '/signup', component: SignUpForm}, 
      {path: '/login', name: 'LoginForm', component: LoginForm}, 
      {path: '/users/:id(\\d+)', name: 'UserDetailPage', component: UserDetailPage}, 
