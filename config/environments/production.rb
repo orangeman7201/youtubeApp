608a7445edef76677b5d002e1f4fb3d1b790afaf
orangeman7201 <aki.jazz1027@gmail.com>
2022-09-15 01:00:28 +0900

    hostを設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  host = 'ystopper.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = host
 
   # Code is not reloaded between requests.
   config.cache_classes = true
