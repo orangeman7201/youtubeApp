625fa190aaee1f5717248ed39f4535ca7186fd68
orangeman7201 <aki.jazz1027@gmail.com>
2022-10-15 00:07:55 +0900

    シリアライザーでdurationを返すようにする

--- app/serializers/friend_serializer.rb
+++ app/serializers/friend_serializer.rb
@@ -1,7 +1,11 @@
 class FriendSerializer < ActiveModel::Serializer
-  attributes :name, :limit, :image_url
+  attributes :name, :limit, :image_url, :duration
 
   def image_url
     object.image_url
+  end
+
+  def duration
+    object.movies.where(date: Date.today.beginning_of_day .. Date.today.end_of_day).sum(:duration)
   end
 end
