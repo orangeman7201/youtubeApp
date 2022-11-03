ccd2fc3af91cbd350aff9f8342c3a6364500e42c
orangeman7201 <aki.jazz1027@gmail.com>
2022-11-03 17:25:48 +0900

    スタイル適用

--- app/javascript/components/modules/FriendRequest.vue
+++ app/javascript/components/modules/FriendRequest.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <div>{{ date }}</div>
-    <div>{{ request.name }}さんからフレンド申請が届きました。</div>
-    <FriendInfoBox :user="request" />
-    <div>
-      <ButtonBase color="#949494" class="mr-7" >キャンセル</ButtonBase>
-      <ButtonBase color="#E8730E" >保存</ButtonBase>
+    <div class="mb-3">
+      <div class="date-text">{{ date }}</div>
+    </div>
+    <div class="request-description">『{{ request.name }}』さんからフレンド申請が届きました</div>
+    <div class="px-6">
+      <FriendInfoBox :user="request" />
+      <div class="d-flex justify-space-between">
+        <ButtonBase color="#949494">キャンセル</ButtonBase>
+        <ButtonBase color="#E8730E">保存</ButtonBase>
+      </div>
     </div>
   </div>
 </template>
@@ -37,15 +41,12 @@ export default {
 </script>
 
 <style scoped>
-.border {
-  border: 0.5px solid #1995AD;
-  background-color: #1995AD;
+.date-text {
+  font-size: 14px;
 }
-.liner1 {
-  flex-basis: 75%;
-}
-.liner2 {
-  flex-basis: 25%;
+.request-description {
+  font-size: 12px;
+  margin-bottom: 30px;
 }
 </style>
 
