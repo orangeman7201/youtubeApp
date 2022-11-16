09b3e8e9bb0f6f13ace00ee5314ce77eac5711c0
orangeman7201 <aki.jazz1027@gmail.com>
2022-11-17 00:21:27 +0900

    lintの修正

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -7,7 +7,7 @@ class FriendsController < ApplicationController
   def create
     if request = Request.find_by(from_user_id: params[:id], to_user_id: current_user.id)
       Friend.create(user_id: current_user.id, friend_user_id: params[:id])
-      Friend.create(user_id: params[:id],         friend_user_id: current_user.id)
+      Friend.create(user_id: params[:id],     friend_user_id: current_user.id)
       request.destroy
     else 
       render json: { errors: [exception] }, status: 404
