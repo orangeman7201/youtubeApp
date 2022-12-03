14fb43a953d3bf0bfefbaa5f196fe9f6075ca297
orangeman7201 <aki.jazz1027@gmail.com>
2022-12-03 16:20:25 +0900

    確認画面を作成

--- app/javascript/components/Pages/signup/steps/Confirm.vue
+++ app/javascript/components/Pages/signup/steps/Confirm.vue
@@ -1,16 +1,113 @@
 <template>
   <div>
-    confirm
+    <div class="header">
+      登録内容の確認
+    </div>
+    <div class="confirm-body px-4">
+      <div class="mb-4">
+        <div class="confirm-detail-header">
+          <img src="~access_time_small.svg" art="" width="24px" height="24px" class="mr-2 mb-2">
+          <span>1日あたりの目標時間</span>
+        </div>
+        {{ limit / 60 }}分
+      </div>
+      <div class="mb-4">
+        <div class="confirm-detail-header">
+          <img src="~face_small.svg" art="" width="24px" height="24px" class="mr-2 mb-2">
+          <span>ニックネーム</span>
+        </div>
+        {{ nickname }}
+      </div>
+      <div class="mb-4">
+        <div class="confirm-detail-header">
+          <img src="~email_small.svg" art="" width="24px" height="24px" class="mr-2 mb-2">
+          <span>メールアドレス</span>
+        </div>
+        {{ email }}
+      </div>
+      <div class="mb-4">
+        <div class="confirm-detail-header">
+          <img src="~disabled_visible_small.svg" art="" width="24px" height="24px" class="mr-2 mb-2">
+          <span>パスワード</span>
+        </div>
+        {{ password }}
+      </div>
+    </div>
+    <button class="submit-button">登録</button>
   </div>
 </template>
 
 <script>
+import RoundedButtonBase from '../../../modules/RoundedButtonBase.vue'
+
 export default {
-  computed: {},
+  components: { RoundedButtonBase },
+  props: {
+    limit: {
+      type: Number,
+      default: undefined,
+    },
+    nickname: {
+      type: String,
+      default: "",
+    },
+    email: {
+      type: String,
+      default: "",
+    },
+    password: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    validPassword() {
+      if(this.password.length > 0 && this.password == this.passwordConfirmation) {
+        return "active-button"
+      }
+      return "disable-button"
+    }
+  },
   methods: {
+    changeParentValueWithPath(event) {
+      this.$emit('change', event.target.value, this.$route.path)
+    },
+    changeParentPasswordConfirmationValue(event) {
+      this.$emit('change', event.target.value, "/signin/passwordConfirmation")
+    },
   }
 }
 </script>
 
 <style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  font-weight: 700;
+  font-size: 16px;
+  margin-bottom: 33px;
+}
+.confirm-body {
+  font-weight: 400px;
+  font-size: 16px;
+  margin-bottom: 40px
+}
+.confirm-detail-header {
+  display: flex;
+  align-items: center;
+  color: #949494;
+}
+.submit-button {
+  width: 80%;
+  height: 44px;
+  padding: 12px 2px;
+  color: white;
+  display: block;
+  text-decoration: none;
+  text-align: center;
+  border-radius: 100px;
+  margin: 0 auto 26px;
+  background-color: #1995ad;
+}
 </style>
+
