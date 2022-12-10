309ae640ae6b18e9829f8cde6b80e2f83e33c2a7
orangeman7201 <aki.jazz1027@gmail.com>
2022-12-10 16:23:01 +0900

    userモデルのテスト

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'User', type: :model do
+  describe 'today' do
+    before do
+      create :movie, user: user, created_at: Date.today
+      create :movie, user: user, created_at: Date.today - 1.day
+      create :movie, user: user, created_at: Date.today + 1.day
+    end
+
+    let(:user) { create :user }
+
+    it '今日作成されたものだけ取得できること' do
+      expect(Movie.today.count).to eq 1
+    end
+  end
+
+  describe 'this_week' do
+    before do
+      create :movie, user: user, created_at: Date.today
+      create :movie, user: user, created_at: Date.today - 7.day
+      create :movie, user: user, created_at: Date.today + 1.day
+    end
+
+    let(:user) { create :user }
+
+    it '先週～今日までに作成されたものだけ取得できること' do
+      expect(Movie.this_week.count).to eq 2
+    end
+  end
+end
