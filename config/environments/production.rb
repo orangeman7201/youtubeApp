63353e2e4655645b4fc86823d0a53d30b0f0a562
AkitaYuma <yuma.akita@smart-sou.co.jp>
2023-02-26 20:28:24 +0900

    active_storageの設定を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  host = 'ystopper.herokuapp.com'
+  host = 'ystopper.com'
   Rails.application.routes.default_url_options[:host] = host
 
   # Code is not reloaded between requests.
